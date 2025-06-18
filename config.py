2c8b8eb82eaea6ed8e1bf1edea5d0cb7427a2d22
iwa999 <armatis@inbox.ru>
2025-06-18 21:53:02 +0500

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-# config.py - Обновленная конфигурация
-import os
-
-# Получение токена из переменных окружения Railway
-TOKEN = os.getenv('TELEGRAM_TOKEN', '7437623986:AAFj-sItRC4s889Sop2mglRE7SE2c-drTCY')
-BOT_USERNAME = '@dsekb_assistant_bot'
